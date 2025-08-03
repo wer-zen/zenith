467546805e44a73e6e94df803f43cb4d4318e2f1
wer-zen <zenwerden@gmail.com>
2025-08-03 12:03:01 +0200

    lots of stuffy

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -3,26 +3,7 @@
 
 {
   imports = [
-    ./quickshell.nix
     ../paths/flow-path.nix
     ./niri.nix
   ]; # global
-  environment.systemPackages = [ pkgs.git pkgs.nixvim ];
-
-  environment.variables.EDITOR = "nvim";
-  environment.variables.MANPAGER = "nvim +Man!";
-  programs.nano.enable = false;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  security.sudo = {
-    execWheelOnly = true;
-    extraRules = [{
-      users = [ "zen" ];
-      # lets me rebuild without having to enter the password
-      commands = [{
-        command = "/run/current-system/sw/bin/nixos-rebuild";
-        options = [ "SETENV" "NOPASSWD" ];
-      }];
-    }];
-  };
 }
--- modules/desktop/quickshell.nix
+++ modules/desktop/quickshell.nix
@@ -10,7 +10,6 @@ in {
   config = mkIf cfg.enable {
     qt.enable = true;
 
-      };
     };
   };
 
