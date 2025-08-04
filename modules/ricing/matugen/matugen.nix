5686a243e06f847cec4b547956de8756047615a1
wer-zen <zenwerden@gmail.com>
2025-08-04 21:58:45 +0200

    matugen

--- modules/ricing/matugen/matugen.nix
+++ modules/ricing/matugen/matugen.nix
@@ -2,8 +2,8 @@
 # https://github.com/InioX/matugen
 { pkgs, lib, config, ... }@args:
 let
-  cfg = config.zen.modules.matugen.matugen;
-  osCfg = args.osConfig.zen.modules.matugen.matugen or { };
+  cfg = config.zen.modules.ricing.matugen.matugen;
+  osCfg = args.osConfig.zen.modules.ricing.matugen.matugen or { };
 
   hexColorRegex = "#([0-9a-fA-F]{3}){1,2}";
   hexStrippedColorRegex = "([0-9a-fA-F]{3}){1,2}";
