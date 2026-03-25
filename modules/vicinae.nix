b017cb7ab353ac99bd3f7b06a30b30b7f67881bb
wer-zen <zenwerden@gmail.com>
2026-03-25 22:36:34 +0100

    changes

--- modules/vicinae.nix
+++ modules/vicinae.nix
@@ -1,5 +1,6 @@
 {
   stdenv,
+  lib,
   fetchurl,
   autoPatchelfHook,
   qt6,
@@ -13,44 +14,48 @@
 }:
 stdenv.mkDerivation rec {
   pname = "vicinae";
-  version = "0.0.5"; # Update to latest
+  version = "0.16.9"; # Check https://github.com/vicinaehq/vicinae/releases for latest
 
   src = fetchurl {
-    url = "https://github.com/vicinaehq/vicinae/releases/download/v${version}/vicinae-linux-x86_64-v${builtins.replaceStrings ["."] [""] version}.tar.gz";
-    sha256 = "0mg6pwqvsk7ggd57vrxvm1fa9sxd6bnw6naz62vyfxxg07g35dd2"; # Update hash
+    url = "https://github.com/vicinaehq/vicinae/releases/download/v${version}/vicinae-linux-x86_64.tar.gz";
+    sha256 = lib.fakeSha256; # Will fail first time - see below
   };
 
-  nativeBuildInputs = [autoPatchelfHook qt6.wrapQtAppsHook];
-  buildInputs = with qt6; [
-    qtbase
-    qtsvg
-    qttools
-    qtwayland
-    qtdeclarative
-    qt5compat
-    kdePackages.qtkeychain
-    kdePackages.layer-shell-qt
-    openssl
-    cmark-gfm
-    libqalculate
-    minizip
-    stdenv.cc.cc.lib
-    abseil-cpp
-    protobuf
-  ];
+  nativeBuildInputs = [autoPatchelfHook];
+  buildInputs = with qt6;
+    [
+      qtbase
+      qtsvg
+      qtdeclarative
+      qtwayland
+    ]
+    ++ (with kdePackages; [
+      layer-shell-qt
+      qtkeychain
+    ])
+    ++ [
+      openssl
+      cmark-gfm
+      libqalculate
+      minizip
+      stdenv.cc.cc.lib
+      abseil-cpp
+      protobuf
+    ];
 
-  unpackPhase = "tar -xzf $src";
+  unpackPhase = "tar -xzf $src --strip-components=1";
   installPhase = ''
-    mkdir -p $out/bin $out/share/applications
-    cp bin/vicinae $out/bin/
-    cp share/applications/vicinae.desktop $out/share/applications/
+    mkdir -p $out/bin $out/share/applications $out/share/icons
+    cp -r bin/* $out/bin/
+    cp -r share/* $out/share/ 2>/dev/null || true
     chmod +x $out/bin/vicinae
   '';
 
-  meta = {
-    description = "Focused launcher for desktop";
+  meta = with lib; {
+    description = "Raycast-like launcher for Linux";
     homepage = "https://github.com/vicinaehq/vicinae";
-    license = stdenv.lib.licenses.gpl3;
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+    maintainers = [];
   };
 }
